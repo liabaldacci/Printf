88e1a868911b4fd5374e2b8029388eb8394e5c5d
liabaldacci <liabaldacci@outlook.com>
2020-05-19 18:28:42 -0300

    Hello Kitty e pft passando

--- ft_check_flags.c
+++ ft_check_flags.c
@@ -1,11 +1,19 @@
 #include "ft_printf.h"
 
-int		ft_check_flags(t_pf *det)
+int			ft_check_flags(t_pf *det)
 {
+	int		i;
+	char	*temp;
+	i = 0;
 	if (ft_star(det) < 0)
 		return (-1);
 	if (ft_negative(det) < 0)
 		return (-1);
+	while (det->flag[i] == '0' || det->flag[i] == '-')
+		i++;
+	temp = det->flag;
+	det->flag = ft_substr(det->flag, i, ft_strlen(det->flag));
+	free(temp);
 	if (ft_width(det) < 0)
 		return (-1);
 	if (ft_precision(det) < 0)
